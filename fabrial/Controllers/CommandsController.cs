c0e2b3d232aef50be59e0abef1aadd2c68972af3
Andrei Chelariu <andreichelariu92@yahoo.com>
2023-06-05 15:40:42 +0300

    Add relevant files for ASP.net project.

---
+++ fabrial/Controllers/CommandsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fabrial.Controllers
+{
+    [Route("[controller]/v1")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpPost("default")]
+        public Task<IActionResult> CreateDefaultCommand()
+        {
+            var result = Created("default", null);
+            return Task.FromResult((IActionResult)result);
+        }
+
+        [HttpPost("custom")]
+        public Task<IActionResult> CreateCustomCommand()
+        {
+            var result = Created("custom", null);
+            return Task.FromResult((IActionResult)result);
+        }
+    }
+}
